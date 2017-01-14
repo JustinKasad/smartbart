0c3f597d5b7761e2d9a1565a48b69fd1d675d528
Justin <justinkasad@gmail.com>
2017-01-13 21:03:00 -0800

    multiple calls but takes too long

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -18,6 +18,7 @@
  */
 var timesArray = [];
 var scroller;
+var count = 0;
 
 var app = {
     // Application Constructor
@@ -85,12 +86,12 @@ var app = {
 
     },
 
-    fetchTrainTimes: function(times){
+    fetchTrainTimes: function(times, start){
         var s = times.split(' ');
         if(s[0] == s[1]){
             return;
         }
-        var url = "http://api.bart.gov/api/sched.aspx?cmd=depart&b=4&a=4&orig="+s[0]+"&dest="+s[1]+"&key=MW9S-E7SL-26DU-VV8V"
+        var url = "http://api.bart.gov/api/sched.aspx?cmd=depart&b=0&a=4&orig="+s[0]+"&dest="+s[1]+"&time="+start+"&key=MW9S-E7SL-26DU-VV8V"
         $.ajax({
                  type: "GET",
                  url: url,
@@ -99,15 +100,35 @@ var app = {
                  contentType: "text/xml; charset=\"utf-8\"",
                  complete: function(xml) {
                     var result = xmlToJSON.parseString(xml.responseText);
-                    timesArray = result.root["0"].schedule["0"].request["0"].trip;
-                    app.displayTimes(timesArray);
+
+                    timesArray.push.apply(timesArray, result.root["0"].schedule["0"].request["0"].trip);
+                    if( app.checklastTrain(result.root["0"].schedule["0"].request["0"].trip[3]._attr.origTimeMin._value)){
+                        app.displayTimes(timesArray);
+                    } else {
+                        var newTime = result.root["0"].schedule["0"].request["0"].trip[3]._attr.origTimeMin._value;
+                        newTime = newTime.split(" ");
+                        var temp = newTime[0].split(':');
+                        if(parseInt(temp[1]) == 59){
+                            temp[0] = parseInt(temp[0]) + 1;
+                        } else {
+                            temp[1] = parseInt(temp[1]) + 1;
+                            if(temp[1].toString().length == 1){
+                                temp[1] =  "0" + temp[1].toString()
+                            }
+                        }
+                        newTime = temp.join(':') + newTime[1].toString().toLowerCase();
+                        setTimeout(function(){
+                            app.fetchTrainTimes(times, newTime);
+                        }, 200)
+                    }
+
                  }
             });
 
     },
     displayTimes: function(trips){
-        $('.times ul').empty()
         var i = 0;
+        $('.spinner').fadeOut();
         $.each(trips, function(key, val){
             var html = '<li><a data-panel="right" href="#" class="open-panel item-content item-link time" data-index='+i+'><div class="item-inner"><div class="item-title">' + val._attr.origTimeMin._value + ' - ' + val._attr.destTimeMin._value + '</div><div class="item-after">'+val._attr.tripTime._value+'m';
             i++;
@@ -130,8 +151,21 @@ var app = {
         $('#bartInput').on('change', function(){
             var s = $(this).val();
             localStorage.setItem('stations', s);
-            app.fetchTrainTimes(s);
+            timesArray = [];
+            $('.times ul').empty();
+            $('.spinner').fadeIn();
+            app.fetchTrainTimes(s, '4:00am');
         })
 
+    },
+    checklastTrain: function(time){
+        console.log("lastTrain: " + time)
+        time = time.split(' ');
+        var temp = time[0].split(':');
+        if(time[1].toLowerCase() == 'am' && (parseInt(temp[0]) == 12 || parseInt(temp[0]) == 1)){
+            return true;
+        } else {
+            return false;
+        }
     }
 };
